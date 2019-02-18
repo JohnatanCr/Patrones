786ab92170faa11c10d1d6c506ac09f59557f730
JohnatanCr <45673177+JohnatanCr@users.noreply.github.com>
2019-02-18 09:24:08 -0600

    Add files via upload

---
+++ mvvm/MVVM_Modelo/Model/TextoCon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Modelo.Model
+{
+    class TextoCon
+    {
+        private readonly Func<string, string> _conversion;
+
+        public TextoCon(Func<string, string> conversion)
+        {
+            _conversion = conversion;
+        }
+
+        public string ConvertText(string Texto)
+        {
+            return _conversion(Texto);
+        }
+    }
+}
+
---
+++ mvvm/MVVM_Modelo/ModelView/Presenter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVM_Modelo.Model;
+
+namespace MVVM_Modelo.ModelView
+{
+    public class Presenter : MasterVM
+    {
+        private readonly TextoCon _textoCon = new TextoCon( text=>text.ToUpper() );
+
+        private string _inputText;
+
+        private readonly ObservableCollection<string> _historial = new ObservableCollection<string>();
+
+        public string InputText
+        {
+            get { return _inputText; }
+            set
+            {
+                _inputText = value;
+                RaisePropertyChangedEvent("InputText");
+            }
+        }
+
+        public IEnumerable<string> Historial
+        {
+            get { return _historial; }
+        }
+
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(ConvertText); }
+        }
+
+        private void ConvertText()
+        {
+            if (string.IsNullOrWhiteSpace(InputText)) return;
+            AddToHistory(_textoCon.ConvertText(InputText));
+            InputText = string.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_historial.Contains(item))
+                _historial.Add(item);
+        }
+    }
+}
+
