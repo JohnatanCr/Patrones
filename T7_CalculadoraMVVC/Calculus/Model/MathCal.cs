55ffcb7623e38e6af5ffd687712fa1e8ec039f1f
JohnatanCr <45673177+JohnatanCr@users.noreply.github.com>
2019-02-19 09:47:11 -0600

    Add files via upload

---
+++ T7_CalculadoraMVVC/Calculus/Model/MathCal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculus.Model
+{
+    public class MathCal
+    {
+        private char var1;
+        private char var2;
+
+        public MathCal(char var1, char var2)
+        {
+            this.var1 = var1;
+            this.var2 = var2;
+        }
+        public MathCal(): this('0', '0')
+        { }
+        
+    }
+
+}
---
+++ T7_CalculadoraMVVC/Calculus/ViewModel/CalculusViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Calculus.Model;
+
+namespace Calculus.ViewModel
+{
+    class CalculusViewModel : ObservableObject
+    {
+        private MathCal Calc = new MathCal();
+        public string calcText { get; set; }
+
+
+
+        public ICommand _AddCalcText
+        {
+            get { return new DelegateCommand(AddCalcText); }
+        }
+    
+        private void AddCalcText()
+        {
+            
+            calcText = "ASDSADS";
+            MessageBox.Show("succed");
+        }
+    }
+}
+
